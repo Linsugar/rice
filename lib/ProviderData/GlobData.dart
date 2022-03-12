c0aabf9f069ae568f35c1c5ad3050c5fb45a6990
PC <1>
2022-03-12 18:05:45 +0800

    更新登录model-在model中不要使用static不然会导致无法使用-

--- lib/ProviderData/GlobData.dart
+++ lib/ProviderData/GlobData.dart
@@ -3,7 +3,7 @@ import '../Models/modeData.dart';
 
 class GlobalData{
 
-  static final StateProvider valueString = StateProvider((ref) => "x");
+  static final StateProvider valueString = StateProvider((ref) => "hello");
   static final StateProvider loginStatue = StateProvider((ref) => false);
   static final StateProvider ProtocolStatue = StateProvider((ref) => true);
   static final StateProvider LoginResult = StateProvider((ref) =>null);
